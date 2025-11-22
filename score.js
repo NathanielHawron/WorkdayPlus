5798d2a35c2d611bba41775f5532150afac8a49e
Jango1324 <Jangomeirbek1324@gmail.com>
2025-11-22 14:28:57 -0800

    Added Api Calls

--- score.js
+++ score.js
@@ -1,5 +1,22 @@
 let scoreButton = document.getElementById("ScoreEnable");
 
 scoreButton.onclick = ()=>{
-    alert("score");
+    getDataRMP();
 };
+
+
+//THE YEAR, TERM, Subject, Level, Section will be provided by nathan
+function getDataUBCGRADES(Year, Term, Subject, Level, Section){ 
+        fetch('heeps://ubcgrades.com/api/v3/grades/UBCO/' + Year + Term + '/' + Subject + '/' + Level + '/' + Section)
+        .then(response => response.json()) // Parse the response body as JSON
+        .then(data => {
+            // Data is now available as a JavaScript object
+            console.log(data);
+            alert(JSON.stringify(data));
+             // Call a function to display the data
+        })
+        .catch(error => console.error('Error fetching data:', error));
+}
+
+
+
