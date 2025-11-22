6e7fafe5aee1ca5f2f19d8a337897c5df1756ab2
Nathaniel <100041819+NathanielHawron@users.noreply.github.com>
2025-11-22 15:59:20 -0800

    Sends api request for courses

--- score.js
+++ score.js
@@ -1,13 +1,36 @@
-console.log("score.js")
+function score(){
+    let year = new Date().getYear() + 1900;
 
-scoreButton.onclick = ()=>{
-    getDataRMP();
+    let list = document.getElementById("wd-FacetedSearchResultList-6$8104").children[2].children[0].children;
+    for(let li of list){
+        let item = li.children[0].children;
+        let button = document.createElement("button");
+        button.innerText = "Get Average";
+
+        let title = item[0].children[0].children[0].children[0].children[0].children[0].children[0];
+        title.appendChild(button);
+        button.onclick = () =>{
+            let string = title.children[1].innerText;
+            let faculty = string.split(" ")[0];
+            faculty = faculty.substring(0,faculty.length-2);
+            let section = string.split(" ")[1];
+            let code = section.substring(0,section.indexOf("-"));
+            section = section.substring(section.indexOf("-")+1,section.length);
+            if(section.length < 3){
+                section = "OVERALL";
+            }
+            console.log(year + "", "W", faculty, code, section);
+            getDataUBCGRADES(year-1 + "", "W", faculty, code, section);
+            getDataUBCGRADES(year-1 + "", "S", faculty, code, section);
+            getDataUBCGRADES(year-2 + "", "W", faculty, code, section);
+            getDataUBCGRADES(year-2 + "", "S", faculty, code, section);
+        }
+    }
 };
 
-
-//THE YEAR, TERM, Subject, Level, Section will be provided by nathan
-function getDataUBCGRADES(Year, Term, Subject, Level, Section){ 
-        fetch('heeps://ubcgrades.com/api/v3/grades/UBCO/' + Year + Term + '/' + Subject + '/' + Level + '/' + Section)
+// Example URL: https://ubcgrades.com/api/v3/grades/UBCO/2022S/MATH/100/OVERALL
+function getDataUBCGRADES(year, term, subject, code, section){
+        fetch('https://ubcgrades.com/api/v3/grades/UBCO/' + year + term + '/' + subject + '/' + code + '/' + section)
         .then(response => response.json()) // Parse the response body as JSON
         .then(data => {
             // Data is now available as a JavaScript object
@@ -17,6 +40,3 @@ function getDataUBCGRADES(Year, Term, Subject, Level, Section){
         })
         .catch(error => console.error('Error fetching data:', error));
 }
-
-
-
