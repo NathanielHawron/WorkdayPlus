923e9bbbdcda5e03e3d485e90755c614dee4107a
Nathaniel <100041819+NathanielHawron@users.noreply.github.com>
2025-11-22 14:29:20 -0800

    Listed courses in course selection page

--- score.js
+++ score.js
@@ -1,5 +1,9 @@
 console.log("score.js")
 
 function score(){
-    console.log("Running scoring");
+    let list = document.getElementById("wd-FacetedSearchResultList-6$8104").children[2].children[0].children;
+    for(let li of list){
+        let item = li.children[0].children;
+        console.log(item);
+    }
 }
