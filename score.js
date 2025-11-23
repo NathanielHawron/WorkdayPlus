7216b7d04bfe6a9c3212784d904a5c5a2037fdc1
Nathaniel <100041819+NathanielHawron@users.noreply.github.com>
2025-11-22 17:50:33 -0800

    Added response for get grades button

--- score.js
+++ score.js
@@ -1,29 +1,79 @@
 function score(){
-    let year = new Date().getYear() + 1900;
-
     let list = document.getElementById("wd-FacetedSearchResultList-6$8104").children[2].children[0].children;
     for(let li of list){
+        /*
+            STATES:
+            0 - Get Average
+            1 - W
+            2 - S
+            3 - Not found
+        */
+        let state = 0;
         let item = li.children[0].children;
         let button = document.createElement("button");
         button.innerText = "Get Average";
 
+        let display = document.createElement("p");
+
+        let dataStringW = "";
+        let dataStringS = "";
+
         let title = item[0].children[0].children[0].children[0].children[0].children[0].children[0];
         title.appendChild(button);
         button.onclick = () =>{
-            let string = title.children[1].innerText;
-            let faculty = string.split(" ")[0];
-            faculty = faculty.substring(0,faculty.length-2);
-            let section = string.split(" ")[1];
-            let code = section.substring(0,section.indexOf("-"));
-            section = section.substring(section.indexOf("-")+1,section.length);
-            if(section.length < 3){
-                section = "OVERALL";
+            switch(state){
+                case 0:{
+                    let string = title.children[1].innerText;
+                    let faculty = string.split(" ")[0];
+                    faculty = faculty.substring(0,faculty.length-2);
+                    let section = string.split(" ")[1];
+                    let code = section.substring(0,section.indexOf("-"));
+                    section = section.substring(section.indexOf("-")+1,section.length);
+                    if(section.length < 3){
+                        section = "OVERALL";
+                    }
+                    
+                    console.log(faculty, code, section);
+                    
+                    let data = getLatestUBCGRADES(faculty, code, section);
+                    if(data.error == "Not Found"){
+                        data = null;
+                    }
+                    // getDataUBCGRADES(year-2 + "", "S", faculty, code, section);
+        
+                    console.log(data);
+        
+                    dataStringS = "Summer data string";
+                    dataStringW = "Winter data string";
+        
+                    if(data.summer != null){
+                        button.innerText = "S";
+                        display.innerText = dataStringS;
+                        state = 2;
+                        title.appendChild(display);
+                    }else if(data.winter != null){
+                        button.innerText = "W";
+                        display.innerText = dataStringW.
+                        state = 1;
+                        title.appendChild(display);
+                    }else{
+                        button.innerText = "Data not available"
+                        state = 3;
+                    }
+                }break;
+                case 1:{
+                    button.innerText = "S";
+                    display.innerText = dataStringS;
+                    state = 2;
+                }break;
+                case 2:{
+                    button.innerText = "W";
+                    display.innerText = dataStringW.
+                    state = 1;
+                }break;
+                default:{
+                }break;
             }
-            console.log(year + "", "W", faculty, code, section);
-            getDataUBCGRADES(year-1 + "", "W", faculty, code, section);
-            getDataUBCGRADES(year-1 + "", "S", faculty, code, section);
-            getDataUBCGRADES(year-2 + "", "W", faculty, code, section);
-            getDataUBCGRADES(year-2 + "", "S", faculty, code, section);
         }
     }
 };
